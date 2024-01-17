a98d57d4c71525b9caaf1dde34713757f2e5bc31
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2024-01-17 23:30:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function calculateGrade(percentage) {
   //your code here
+	if(percentage >= 90){
+		return "A";
+	}
+	else if(percentage <=89 && percentage >=80){
+		return "B";
+	}
+	else if(percentage <=79 && percentage >=70){
+		return "C";
+	}
+	else if(percentage <=69 && percentage >=60){
+		return "D";
+	}
+	else{
+		return "F";
+	}
 }
 const percentage = parseInt(prompt("Enter Percentage."));
 alert(calculateGrade(percentage));
